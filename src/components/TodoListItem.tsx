15b760f086a0114a964a8dbef4b97d514a1cde10
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-09-25 23:47:13 +0300

    Updated TodoListItem component to use MUI components and added delete icon functionality

--- src/components/TodoListItem.tsx
+++ src/components/TodoListItem.tsx
@@ -1,13 +1,35 @@
-import React, { FC } from 'react'
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import DeleteIcon from "@mui/icons-material/Delete";
+import IconButton from "@mui/material/IconButton";
+import { FC } from "react";
 
 interface ITodoListItem extends ITodoListFn {
-    todos: ITodoType;
-  }
-
-const TodoListItem:FC<ITodoListItem> = ({todo, toggleTodo, deleteTodo}) => {
-  return (
-    <div>TodoListItem</div>
-  )
+  todos: ITodoType;
 }
 
-export default TodoListItem
+const TodoListItem: FC<ITodoListItem> = ({ todo, toggleTodo, deleteTodo }) => {
+  return (
+    <ListItem
+      disableGutters
+      sx={{ padding: "1rem", cursor: "pointer" }}
+      secondaryAction={
+        <IconButton
+          sx={{ "&:hover": { color: "red" } }}
+          aria-label="Delete"
+          onClick={() => deleteTodo(todo.id)}
+        >
+          <DeleteIcon />
+        </IconButton>
+      }
+    >
+      <ListItemText
+        sx={{ wordWrap: "break-word" }}
+        primary={todo.task}
+        onClick={() => toggleTodo(todo)}
+      />
+    </ListItem>
+  );
+};
+
+export default TodoListItem;
