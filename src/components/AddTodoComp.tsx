bf3f7d5c219d693b8f7c72e1884c8cfe357b3f63
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-09-26 00:28:09 +0300

    Updated AddTodoComp component with new styling and formatting

--- src/components/AddTodoComp.tsx
+++ src/components/AddTodoComp.tsx
@@ -3,10 +3,10 @@ import SaveIcon from "@mui/icons-material/Save";
 import { useState } from "react";
 
 interface IAddTodoComp {
-  addTodo: AddFn
+  addTodo: AddFn;
 }
 
-const AddTodoComp = ({addTodo}: IAddTodoComp) => {
+const AddTodoComp = ({ addTodo }: IAddTodoComp) => {
   // const [text, setText] = useState<string>("") // initial value is String
   const [text, setText] = useState("");
 
@@ -16,18 +16,27 @@ const AddTodoComp = ({addTodo}: IAddTodoComp) => {
   };
 
   return (
-    <Box>
+    <Box
+      sx={{
+        display: { xs: "block", sm: "flex" },
+        justifyContent: { xs: "flex-start", sm: "center" },
+        m: { xs: 1, sm: "auto" },
+        height: { xs: "120px", sm: "80px" },
+      }}
+    >
       <TextField
         id="outlined-disabled"
         label="New Todo"
         variant="outlined"
         value={text}
         onChange={(e) => setText(e.target.value)}
+        sx={{ minWidth: { xs: "100%", sm: "50%" }, height: "50px", m: 1 }}
       />
       <Button
         onClick={handleClick}
         variant="contained"
         color="success"
+        sx={{ minWidth: { xs: "100%", sm: "15%" }, height: "55px", m: 1 }}
         startIcon={<SaveIcon />}
         disabled={!text.trim()}
       >
